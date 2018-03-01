1733a5707ea5e768c3c82e18ae4b677a6295d895
Benjamin <benja.mora.torres@gmail.com>
2018-03-01 14:49:15 -0300

    Login con google, guarda base de datos

---
+++ parte 1/app.js
@@ -0,0 +1,41 @@
+//Login
+var provider = new firebase.auth.GoogleAuthProvider();
+
+$('#login').click(function(){
+  firebase.auth()
+    .signInWithPopup(provider)
+    .then(function(result){
+      console.log(result.user);
+      guardarDatos(result.user);
+      $('#login').hide;
+      $('#root').append("<img src='"+result.user.photoURL+"' />")
+    });
+});
+
+//guardar datos automaticamente
+function guardarDatos(user){
+  var usuario = {
+    uid: user.uid,
+    nombre : user.displayName,
+    email : user.email,
+    foto: user.photoURL
+  }
+  firebase.database().ref("usuarios/"+ user.uid)
+  .set(usuario)
+}
+
+//Escribir una base de datos
+$('#guardar').click(function(){
+  firebase.database().ref("persona")
+  .set({
+    nombre: "benjamin",
+    edad: "22"
+  })
+});
+
+//Leer base de Datos
+firebase.database().ref("usuarios")
+.on("child_added", function(s){
+  var user = s.val();
+  $('#root').append("<img width='100px' src='"+user.foto+"' />")
+});
